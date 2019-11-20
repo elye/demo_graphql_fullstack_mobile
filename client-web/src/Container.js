0fdac8ddb3b1059fcf2723b71d02e14719b1b175
elye <elye.project@gmail.com>
2019-11-20 14:29:50 +1100

    Getting the apollo packages for apollo support on client.

--- client-web/src/Container.js
+++ client-web/src/Container.js
@@ -1,31 +1,55 @@
 import React, {Component} from 'react';
 import SearchBox from "./SearchBox";
 import SearchResult from "./SearchResult";
+import {useQuery} from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
+const GET_WIKI_HIT = gql`
+    query GetWikicountByKeyword($keyword: String!) {
+        wikiCount(keyword: $keyword) {
+            keyword
+            totalhits
+        }
+    }
+`;
 
 export default class Container extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            result: "No Result"
+            keyword: ""
         };
     }
 
     onChange = keyword => {
-        this.setState({result: this.getHitCount(keyword)});
+        this.setState({
+                keyword: this.getHitCount(keyword)
+            }
+        );
     };
 
     getHitCount(keyword) {
-        const hitCount = 100
-        return keyword + ":" + hitCount
+        return keyword
     }
 
     render() {
         return (
             <div>
                 <div><SearchBox onSubmit={(keyword) => this.onChange(keyword)}/></div>
-                <div><SearchResult value={this.state.result}/></div>
+                <div><WikiHit keyword={this.state.keyword}/></div>
             </div>
         );
     }
+}
+
+function WikiHit({keyword}) {
+    const {data, loading, error} = useQuery(GET_WIKI_HIT, {variables: {keyword: keyword}});
+
+    let result
+    if (keyword === "") result ="No Result";
+    else if (loading)  result ="Loading...";
+    else if (error)  result = error.message;
+    else result = data.wikiCount.keyword + ":" + data.wikiCount.totalhits;
+
+    return <div><SearchResult value={result}/></div>;
 }
--- client-web/src/index.js
+++ client-web/src/index.js
@@ -4,7 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+    uri: 'http://localhost:4000/'
+});
+
+const client = new ApolloClient({
+    cache,
+    link
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
