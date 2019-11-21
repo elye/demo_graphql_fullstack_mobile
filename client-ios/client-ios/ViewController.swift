89f907511adbbd1a9f17ab04c1d5cfaa16fca0ff
elye <elye.project@gmail.com>
2019-11-21 21:23:18 +1100

    Change to use local graphql data

--- client-ios/client-ios/ViewController.swift
+++ client-ios/client-ios/ViewController.swift
@@ -33,37 +33,57 @@ class ViewController: UIViewController {
     request?.cancel()
   }
 
+
   private func fetchData(searchText: String) {
 
-    let url = "https://en.wikipedia.org/w/api.php"
+    let url = "http://EDIT_YOUR_HOST_HERE:4000"
+
+    let search = """
+    { wikiCount
+        (keyword: \"\(searchText)\") {
+          keyword
+          totalhits
+        }
+    }
+    """
+
     let parameters: Parameters = [
-      "action": "query",
-      "format": "json",
-      "list": "search",
-      "srsearch": searchText
+      "query": search
     ]
 
     request?.cancel()
-    request = Alamofire.SessionManager.default.request(url, method: .get, parameters: parameters)
+    request = Alamofire.SessionManager.default.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default)
       .responseData { (resData) -> Void in
         self.loadingIndicator.stopAnimating()
 
         switch (resData.result) {
         case let .success(data):
           let swiftyJsonVar = JSON(data)
-          let countObj = swiftyJsonVar["query"]["searchinfo"]["totalhits"]
-          guard let count = countObj.int else {
-            self.showResult(data: "No data found")
-            return
+          let countObj = swiftyJsonVar["data"]["wikiCount"]["totalhits"]
+          let keywordObj = swiftyJsonVar["data"]["wikiCount"]["keyword"]
+          guard
+            let count = countObj.rawValue as? String,
+            let keyword = keywordObj.rawValue as? String,
+            count.isNumber
+            else {
+              self.showResult(data: "No data found")
+              return
           }
-          self.showResult(data: "Count is \(count)")
+          self.showResult(data: "\(keyword):\(count)")
         case let .failure(error):
           self.showResult(data: error.localizedDescription)
         }
     }
+
   }
 
   private func showResult(data: String) {
     labelResult.text = data
   }
+}
+
+extension String  {
+    var isNumber: Bool {
+        return !isEmpty && rangeOfCharacter(from: CharacterSet.decimalDigits.inverted) == nil
+    }
 }
