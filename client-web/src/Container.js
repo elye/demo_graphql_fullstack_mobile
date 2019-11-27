2f1b912b1cdbebcd16eabdff5417a237d7a951c4
elye <elye.project@gmail.com>
2019-11-27 22:32:52 +1100

    Separate out the WikiFetch as a different file

--- client-web/src/Container.js
+++ client-web/src/Container.js
@@ -1,19 +1,7 @@
 import React, {Component} from 'react';
 import SearchBox from "./SearchBox";
-import SearchResult from "./SearchResult";
-import {useQuery} from '@apollo/react-hooks';
-import Loading from "./loading"
-import gql from 'graphql-tag';
 import NormalFetch from "./NormalFetch";
-
-const GET_WIKI_HIT = gql`
-    query GetWikicountByKeyword($keyword: String!) {
-        wikiCount(keyword: $keyword) {
-            keyword
-            totalhits
-        }
-    }
-`;
+import WikiFetch from "./WikiFetch";
 
 export default class Container extends Component {
     constructor(props) {
@@ -38,21 +26,9 @@ export default class Container extends Component {
         return (
             <div>
                 <div><SearchBox onSubmit={(keyword) => this.onChange(keyword)}/></div>
-                <div><WikiHit keyword={this.state.keyword}/></div>
+                <div><WikiFetch keyword={this.state.keyword}/></div>
                 <div><NormalFetch /></div>
             </div>
         );
     }
-}
-
-function WikiHit({keyword}) {
-    const {data, loading, error} = useQuery(GET_WIKI_HIT, {variables: {keyword: keyword}});
-
-    let result
-    if (keyword === "") result ="No Result";
-    else if (loading) return (<Loading />);
-    else if (error)  result = error.message;
-    else result = data.wikiCount.keyword + ":" + data.wikiCount.totalhits;
-
-    return <div><SearchResult value={result}/></div>;
 }
---
+++ client-web/src/WikiFetch.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {useQuery} from "@apollo/react-hooks";
+import SearchResult from "./SearchResult";
+import Loading from "./loading"
+import gql from 'graphql-tag';
+
+const GET_WIKI_HIT = gql`
+    query GetWikicountByKeyword($keyword: String!) {
+        wikiCount(keyword: $keyword) {
+            keyword
+            totalhits
+        }
+    }
+`;
+
+function WikiFetch({keyword}) {
+    const {data, loading, error} = useQuery(GET_WIKI_HIT, {variables: {keyword: keyword}});
+
+    let result
+    if (keyword === "") result ="No Result";
+    else if (loading) return (<Loading />);
+    else if (error)  result = error.message;
+    else result = data.wikiCount.keyword + ":" + data.wikiCount.totalhits;
+
+    return <div><SearchResult value={result}/></div>;
+}
+
+export default WikiFetch
